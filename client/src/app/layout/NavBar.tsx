abff9577adc31594357766e60361b166bd4445b8
Marko Stankovic <stankovicm0808@gmail.com>
2026-02-17 16:04:57 +0100

    End of section 3

---
+++ client/src/app/layout/NavBar.tsx
@@ -0,0 +1,20 @@
+import { AppBar, Typography, Toolbar, IconButton } from "@mui/material";
+import { DarkMode, LightMode } from "@mui/icons-material";
+
+type Props = {
+    toggleDarkMode: () => void;
+    darkMode: boolean;
+}
+
+export default function NavBar({ darkMode, toggleDarkMode }:Props) {
+    return (
+        <AppBar position="fixed">
+            <Toolbar>
+                <Typography variant="h6">Re-STORE</Typography>
+                <IconButton onClick={toggleDarkMode }>
+                    {darkMode ? <DarkMode /> : <LightMode sx={{color:'yellow'}} />}
+                </IconButton>
+            </Toolbar>
+        </AppBar>
+    )
+}
